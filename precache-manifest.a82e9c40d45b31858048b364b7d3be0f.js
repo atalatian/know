515444c0623346a48a215f67db55a537026ceedb
atalatian <amirhoseyntalatyan@gmail.com>
2020-10-17 18:16:17 +0330

    Updates

---
+++ precache-manifest.a82e9c40d45b31858048b364b7d3be0f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd50abadfa9f1f8134f80d7f677478c4",
+    "url": "/%7Bknow%7D/index.html"
+  },
+  {
+    "revision": "2aebd302fe84fb72a1ec",
+    "url": "/%7Bknow%7D/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "84b9eec4a7c3ea295912",
+    "url": "/%7Bknow%7D/static/css/main.c752bef0.chunk.css"
+  },
+  {
+    "revision": "2aebd302fe84fb72a1ec",
+    "url": "/%7Bknow%7D/static/js/2.04c298e5.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/%7Bknow%7D/static/js/2.04c298e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b9eec4a7c3ea295912",
+    "url": "/%7Bknow%7D/static/js/main.4708060e.chunk.js"
+  },
+  {
+    "revision": "245a0c3a2b67673a8c62",
+    "url": "/%7Bknow%7D/static/js/runtime-main.611fc43b.js"
+  },
+  {
+    "revision": "e43684592b179ce7022b3fe0e8a60eb2",
+    "url": "/%7Bknow%7D/static/media/arm.e4368459.png"
+  },
+  {
+    "revision": "ab92436379e2d15899ceaa257a4c5cfb",
+    "url": "/%7Bknow%7D/static/media/branch.ab924363.png"
+  },
+  {
+    "revision": "57cf2f6d819ee49ac88b6653c6d49ea7",
+    "url": "/%7Bknow%7D/static/media/c.57cf2f6d.png"
+  },
+  {
+    "revision": "b2dca7f53dbd7938f83ec7415a67863c",
+    "url": "/%7Bknow%7D/static/media/hamburger.b2dca7f5.jpg"
+  },
+  {
+    "revision": "4432abad53b7a0fe794a48cdafa2111d",
+    "url": "/%7Bknow%7D/static/media/logo.4432abad.png"
+  }
+]);

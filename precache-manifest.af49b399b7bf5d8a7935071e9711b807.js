67adbcebf13fa18f700502c83db5d53b5711bc49
atalatian <amirhoseyntalatyan@gmail.com>
2022-09-10 19:58:08 +0430

    menu_fixed

---
+++ precache-manifest.af49b399b7bf5d8a7935071e9711b807.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4525f70af2129f129d8fa66cbde2d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "991453caab0bedf889c7",
+    "url": "./static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "185e1d75ca511b280b51",
+    "url": "./static/css/main.6dfccbcc.chunk.css"
+  },
+  {
+    "revision": "991453caab0bedf889c7",
+    "url": "./static/js/2.365dcb09.chunk.js"
+  },
+  {
+    "revision": "6bfe880a6f3dba95429b25cd4a2398bd",
+    "url": "./static/js/2.365dcb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185e1d75ca511b280b51",
+    "url": "./static/js/main.2052871b.chunk.js"
+  },
+  {
+    "revision": "c24f9df85abc96f02112",
+    "url": "./static/js/runtime-main.14c5ecec.js"
+  },
+  {
+    "revision": "0f1380d729b4ec737cc09fe9f8ab27f0",
+    "url": "./static/media/Background.0f1380d7.png"
+  },
+  {
+    "revision": "077151e9facf80d34437dec292f83a29",
+    "url": "./static/media/branch.077151e9.png"
+  },
+  {
+    "revision": "7548120f3024aece92ff3ef7227e6e32",
+    "url": "./static/media/breakfast.7548120f.jpg"
+  },
+  {
+    "revision": "e4f2ba74b55d6867cb0eb7ff45042e2c",
+    "url": "./static/media/c.e4f2ba74.png"
+  },
+  {
+    "revision": "1f91d1f776568fcfdeb1ce12d16367ce",
+    "url": "./static/media/chicken.1f91d1f7.jpg"
+  },
+  {
+    "revision": "5971ff1ac6114caca94e8ab2eedff543",
+    "url": "./static/media/dinner.5971ff1a.jpg"
+  },
+  {
+    "revision": "ec9f72b4e074f2c9b11fb6df4f93ff22",
+    "url": "./static/media/hamburger.ec9f72b4.jpg"
+  },
+  {
+    "revision": "89a4960478149dfae0e1cced63875242",
+    "url": "./static/media/honey.89a49604.jpeg"
+  },
+  {
+    "revision": "91269f2bc4dcb22531dc414dfd77f401",
+    "url": "./static/media/ice-cream.91269f2b.jpg"
+  },
+  {
+    "revision": "66c4a6d6e812a3a9e3e7ee3905cde51d",
+    "url": "./static/media/icecream.66c4a6d6.jpg"
+  },
+  {
+    "revision": "1a7c8f637b3ac172ee55fb23e6474759",
+    "url": "./static/media/logo.1a7c8f63.png"
+  },
+  {
+    "revision": "1f4486b5fa7660be0cbc0267aa2d60d6",
+    "url": "./static/media/lunch.1f4486b5.jpg"
+  },
+  {
+    "revision": "7800814707969b60b89fc4b2601fe2d2",
+    "url": "./static/media/pizza.78008147.jpg"
+  },
+  {
+    "revision": "5001914f5878777f56285e8e0db15310",
+    "url": "./static/media/sub-icecream1.5001914f.png"
+  },
+  {
+    "revision": "9cd98769de87cedacc52f755b4204777",
+    "url": "./static/media/sub-icecream2.9cd98769.png"
+  },
+  {
+    "revision": "6eb7ec1b628ca5e4155488c3e3cb0a1b",
+    "url": "./static/media/sub-icecream3.6eb7ec1b.png"
+  },
+  {
+    "revision": "236155ee6be88a1f7ce390418d0a2f67",
+    "url": "./static/media/sub-icecream4.236155ee.png"
+  },
+  {
+    "revision": "394403d19df692d472c71da4d13f2ff7",
+    "url": "./static/media/sub-icecream5.394403d1.png"
+  },
+  {
+    "revision": "74110b3e05dd24c320cdfc4a0212521f",
+    "url": "./static/media/sub-icecream6.74110b3e.png"
+  },
+  {
+    "revision": "7cd9c4c37728fbf545ab88f3519dac0e",
+    "url": "./static/media/sub-lunch1.7cd9c4c3.png"
+  },
+  {
+    "revision": "99d45b6536476027510e4fb536679d9f",
+    "url": "./static/media/sub-lunch2.99d45b65.png"
+  }
+]);
